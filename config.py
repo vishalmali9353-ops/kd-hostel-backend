3b7aa49f772f899399f49d039f6cdaa85a81d899
vishalmali9353-ops <vishalmali.9353@gmail.com>
2026-09-07 08:49:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/kd_hostel")
     FRONTEND_ORIGIN = os.environ.get("FRONTEND_ORIGIN", "*")
+    CLOUDINARY_CLOUD_NAME = os.environ.get("CLOUDINARY_CLOUD_NAME", "")
+    CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY", "")
+    CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET", "")
